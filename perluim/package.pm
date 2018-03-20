e1b4a2138073c9fcf88d0d1870ff13b951347a1a
Thomas GENTILHOMME <Thomas.GENTILHOMME@bnpparibas.com>
2018-03-20 11:20:39 +0100

    Add perluim code & documentation

---
+++ perluim/package.pm
@@ -0,0 +1,32 @@
+use strict;
+use warnings;
+
+# Namespace
+package perluim::package;
+
+# Nimsoft librairies !
+use Nimbus::API;
+use Nimbus::PDS;
+use Nimbus::CFG;
+
+sub new {
+    my ($class,$o) = @_;
+    my $this = {
+        name            => $o->get("name"),
+        description     => $o->get("description") || "",
+        version         => $o->get("version") || "",
+        build           => $o->get("build") || "",
+        date            => $o->get("date") || "",
+        install_date    => $o->get("install_date"),
+        valid           => 1
+    };
+    return bless($this,ref($class) || $class);
+}
+
+sub setValid {
+    my ($self,$vInt) = @_;
+    $self->{valid} = $vInt;
+}
+
+
+1;
